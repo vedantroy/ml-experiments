7765fdb40e0d0ab39551c8d693b3c84a4b6e97b9
Vedant Roy <vroy101@gmail.com>
2022-07-06 20:36:44 -0700

    add pytorch lightning trainer

--- unet/lightning_train.py
+++ unet/lightning_train.py
@@ -1,6 +1,9 @@
-from importlib.resources import is_resource
 from pathlib import Path
 import argparse
+
+import signal
+import sys
+import os
 
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
@@ -10,11 +13,11 @@ from torch import optim
 from torch.utils.data import DataLoader, random_split
 from model import UNet
 import wandb
-from unet.data import BasicDataset
+from data import BasicDataset
 
 
-from unet.loss import dice_loss
-from unet.utils import convert_mask_to_ground_truth
+from loss import dice_loss
+from utils import convert_mask_to_ground_truth
 
 
 def get_losses(batch, model, n_classes):
@@ -33,8 +36,18 @@ def get_losses(batch, model, n_classes):
     return (loss_cross_entropy, loss_dice, combined_loss, (imgs, masks, mask_preds))
 
 
+EXIT_TRAINER = False
+
+
 class LightningModel(pl.LightningModule):
-    def __init__(self, scale: float, percent_of_data_used_for_validation: int, batch_size: int, wandb_logger, run_id: str):
+    def __init__(
+        self,
+        scale: float,
+        percent_of_data_used_for_validation: int,
+        batch_size: int,
+        run_id: str,
+        **kwargs,
+    ):
         super().__init__()
         self.model = UNet(n_in_channels=3, n_classes=2)
         dataset = BasicDataset(
@@ -45,9 +58,10 @@ class LightningModel(pl.LightningModule):
         assert n_train + n_val == len(dataset)
 
         train_set, val_set = random_split(
-                dataset, [n_train, n_val], 
-                # if we use seed_everything, do we still need this?
-                #generator=torch.Generator().manual_seed(0)
+            dataset,
+            [n_train, n_val],
+            # if we use seed_everything, do we still need this?
+            # generator=torch.Generator().manual_seed(0)
         )
         self.train_set = train_set
         self.val_set = val_set
@@ -59,57 +73,86 @@ class LightningModel(pl.LightningModule):
             pin_memory=self.device.type == "cuda",
         )
         self.sample_arg = None
-        self.wandb_logger = wandb_logger
         self.run_id = run_id
-        self.wandb_logger = wandb_logger
+        self.learning_rate = kwargs["learning_rate"]
+        self.early_exit = False
 
     def forward(self, x):
         return self.model(x)
 
     def configure_optimizers(self):
-        optimizer = optim.AdamW(self.parameters(), lr=1e-3)
+        optimizer = optim.AdamW(self.parameters(), lr=self.learning_rate)
         # no LR schedulers
         return [optimizer], []
 
+    def on_train_batch_start(self, batch, batch_idx: int, unused: int = 0):
+        if self.early_exit:
+            return -1
+
     def training_step(self, batch, batch_idx):
-        cross_entropy , dice, combined, _ = get_losses(batch, self, self.model.n_classes)
+        cross_entropy, dice, combined, _ = get_losses(batch, self, self.model.n_classes)
+        wandb.log(
+            {
+                "loss_cross_entropy": cross_entropy,
+                "loss_dice": dice,
+                "loss_combined": combined,
+                "step": trainer.global_step
+            }
+        )
         return combined
 
-    def validation_step(self, batch, batch_idx): 
-        cross_entropy , dice, combined, (imgs, masks, mask_preds) = get_losses(batch, self, self.model.n_classes)
-        if not self.sample_arg:
-            self.sample_arg = imgs[0]
+    def validation_step(self, batch, batch_idx):
+        cross_entropy, dice, combined, (imgs, masks, mask_preds) = get_losses(
+            batch, self, self.model.n_classes
+        )
+        if self.sample_arg == None:
+            self.sample_arg = imgs
 
-        self.log("val_cross_entropy_loss", cross_entropy)
-        self.log("val_dice_loss", dice)
-        self.log("val_combined_loss", combined)
-        wandb.log({
-            "image": wandb.Image(imgs[0].cpu()),
-            "masks": {
-                "true": wandb.Image(masks[0].float().cpu()),
-                "pred": wandb.Image(mask_preds.argmax(dim=1)[0].float().cpu())
-            },
-            "step": trainer.global_step
-        })
+        wandb.log(
+            {
+                "validation_loss_cross_entropy": cross_entropy,
+                "validation_loss_dice": dice,
+                "validation_loss_combined": combined,
+                "step": trainer.global_step
+            }
+        )
+        wandb.log(
+            {
+                "image": wandb.Image(imgs[0].cpu()),
+                "masks": {
+                    "true": wandb.Image(masks[0].float().cpu()),
+                    "pred": wandb.Image(mask_preds.argmax(dim=1)[0].float().cpu()),
+                },
+                "step": trainer.global_step,
+            }
+        )
 
     def train_dataloader(self):
-        return DataLoader(self.train_set, self.batch_size, shuffle=True, **self.loader_args)
+        return DataLoader(self.train_set, shuffle=True, **self.loader_args)
 
     def val_dataloader(self):
-        loader =  DataLoader(self.val_set, shuffle=False, drop_last=True, **self.loader_args)
-        assert len(loader) > 0, "validation loader must have enough data for at least 1 full batch"
+        loader = DataLoader(
+            self.val_set, shuffle=False, drop_last=True, **self.loader_args
+        )
+        assert (
+            len(loader) > 0
+        ), "validation loader must have enough data for at least 1 full batch"
         return loader
 
     def on_train_end(self):
-        path = Path("./runs/{self.run_id}")
+        if self.early_exit:
+            return
+
+        path = Path(f"./runs/{self.run_id}")
         path.mkdir(parents=True, exist_ok=True)
 
-        model_path =  path / "final_model.pt"
+        model_path = path / "final_model.pt"
         model_onnx_path = path / "final_model.onnx"
         torch.save(self.state_dict(), model_path)
         torch.onnx.export(self.model, self.sample_arg, model_onnx_path)
-        wandb.save(model_path)
-        wandb.save(model_onnx_path)
+        wandb.save(str(model_path))
+        wandb.save(str(model_onnx_path))
+
 
 def get_args():
     parser = argparse.ArgumentParser(
@@ -117,12 +160,13 @@ def get_args():
     )
     parser.add_argument(
         "--id",
-        "-R",
+        "-I",
         type=str,
         default=False,
         help="Load model using an existing run id",
     )
     return parser.parse_args()
+
 
 if __name__ == "__main__":
     model = run_id = config = None
@@ -131,12 +175,17 @@ if __name__ == "__main__":
     if args.id:
         is_resuming = True
         run_id = args.id
+        print(f"Resume run with id: {run_id} from checkpoint")
         lighting_checkpoint = Path(f"./runs/{run_id}/lightning.checkpoint")
         wandb_checkpoint = Path(f"./runs/{run_id}/wandb.checkpoint")
         if not lighting_checkpoint.is_file():
-            raise FileNotFoundError(f"Could not find checkpoint at {lighting_checkpoint}")
-        model = LightningModel.load_from_checkpoint(lighting_checkpoint)
+            raise FileNotFoundError(
+                f"Could not find checkpoint at {lighting_checkpoint}"
+            )
         config = torch.load(wandb_checkpoint)
+        model = LightningModel.load_from_checkpoint(
+            lighting_checkpoint, run_id=run_id, **config
+        )
     else:
         run_id = wandb.util.generate_id()
         print(f"Starting new run with id: {run_id}")
@@ -152,20 +201,35 @@ if __name__ == "__main__":
     p = Path(f"./runs/{run_id}")
     p.mkdir(parents=True, exist_ok=True)
 
-    logger = WandbLogger(id=run_id)
-    trainer = pl.Trainer(max_epochs=config['epochs'], logger=logger)
+    #logger = WandbLogger(project="UNet", id=run_id)
+    trainer = pl.Trainer(
+        accelerator="gpu",
+        devices=1,
+        max_epochs=config["epochs"],
+        #logger=logger,
+        enable_checkpointing=False,
+    )
     model = LightningModel(**config, run_id=run_id)
 
-    with wandb.init(project="UNet", id=run_id, resume="must" if is_resuming else "never", config=config):
+    with wandb.init(
+        project="UNet",
+        id=run_id,
+        resume="must" if is_resuming else "never",
+        config=config,
+    ):
         wandb.watch(model.model, criterion=None, log="all", log_freq=100)
 
-        try:
-            trainer.fit(model)
-        except KeyboardInterrupt as e:
-            throw = e
+        def signal_handler(signal, frame):
+            model.early_exit = True
 
-    trainer.save_checkpoint(f"./runs/{run_id}/lightning.checkpoint")
-    torch.save(wandb.config, f"./runs/{run_id}/wandb.checkpoint")
+        signal.signal(signal.SIGINT, signal_handler)
+        trainer.fit(model)
 
-    if throw:
-        raise throw
+    lightning_checkpoint = p / "lightning.checkpoint"
+    trainer.save_checkpoint(lightning_checkpoint)
+    if os.environ.get("WANDB_MODE") != "disabled":
+        torch.save(config, p / "wandb.checkpoint")
+
+    if model.early_exit:
+        print("Received SIGINT")
+        sys.exit(1)
