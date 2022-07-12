984642936bb19ef7cf5628610bf1f47290d0b117
Vedant Roy <vroy101@gmail.com>
2022-07-12 14:06:12 -0700

    add script to dump to sqlite

--- imagen/data/danbooru/scripts/dump_tags_to_sqlite.js
+++ imagen/data/danbooru/scripts/dump_tags_to_sqlite.js
@@ -6,6 +6,7 @@ const assert = require("assert")
 
 const tqdm = require("tqdm")
 const load = require("better-sqlite3")
+const _ = require("lodash")
 
 const DB_PATH = "../data/db.sqlite"
 const CHUNKS_DIR = "../data/chunks"
@@ -27,7 +28,16 @@ async function lines(filePath, { db }) {
     // make it work w/ tqdm
     rdr[Symbol.toStringTag] = "AsyncGenerator";
 
-    const stmt = db.prepare("INSERT INTO tags (id, tag) VALUES (?, ?)")
+    function makeBulkInsertStmt(n) {
+        const bulkInsertParams = _.repeat("(?, ?),", n)
+        // strip the last comma
+        .slice(0, -1)
+        return  db.prepare(`INSERT INTO tags (id, tag) VALUES ${bulkInsertParams}`)
+    }
+
+    const BULK_INSERT = 5_000
+    const bulkInsertStmt = makeBulkInsertStmt(BULK_INSERT)
+    let bulkInsertBuffer = []
     for await (const line of tqdm(rdr, { total: nLines })) {
         const entry = JSON.parse(line)
         const { id, tag_string_general } = entry;
@@ -40,9 +50,18 @@ async function lines(filePath, { db }) {
             skipped += 1
             continue
         }
-        // SQLITE does no type-checking, so theoretically you could insert strings into the int column
-        stmt.run(id, tag_string_general)
+        bulkInsertBuffer.push(id)
+        bulkInsertBuffer.push(tag_string_general)
+        if (bulkInsertBuffer.length === 2 * BULK_INSERT) {
+            bulkInsertStmt.run(...bulkInsertBuffer)
+            bulkInsertBuffer = []
+        }
         inserted += 1
+    }
+    if (bulkInsertBuffer.length > 0) {
+        assert(bulkInsertBuffer.length % 2 === 0, `Invalid length: ${bulkInsertBuffer.length}`)
+        const stmt = makeBulkInsertStmt(bulkInsertBuffer.length / 2)
+        stmt.run(...bulkInsertBuffer)
     }
     return { inserted, skipped }
 }
@@ -74,6 +93,9 @@ async function main() {
         const { skipped, inserted } = await lines(path.join(CHUNKS_DIR, file), { db })
         console.log(`${inserted}/${inserted + skipped} entries inserted`)
     }
+    // merge the WAL file back into the DB
+    // https://stackoverflow.com/questions/19574286/how-to-merge-contents-of-sqlite-3-7-wal-file-into-main-database-file
+    db.exec("VACUUM;")
 }
 
 main()
