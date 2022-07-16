027030555d66d420e7a70f322753b4ccc4affc95
Vedant Roy <vroy101@gmail.com>
2022-07-16 15:50:12 -0700

    before annotating all shapes

---
+++ modern_unet/run_unet.py
@@ -0,0 +1,19 @@
+import torch
+from annotated_unet import UNet
+
+# Let's see it in action on dummy data:
+
+# A dummy batch of 10 3-channel 32px images
+x = torch.randn(10, 3, 32, 32)
+
+# 't' - what timestep are we on
+t = torch.tensor([50], dtype=torch.long)
+
+# Define the unet model
+unet = UNet()
+
+# The foreward pass (takes both x and t)
+model_output = unet(x, t)
+
+# The output shape matches the input.
+model_output.shape
