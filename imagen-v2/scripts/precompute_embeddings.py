67067bd5fc6405bea74c161344d6c3a50be3db9f
Vedant Roy <vroy101@gmail.com>
2022-07-20 04:14:16 -0700

    pre-computing embeds works

--- imagen-v2/scripts/precompute_embeddings.py
+++ imagen-v2/scripts/precompute_embeddings.py
@@ -1,12 +1,16 @@
 from pathlib import Path
 import argparse
-import sqlite3
+from supersqlite import sqlite3
 
+import torch
 from torch.utils.data import DataLoader
 from torch.utils.data.dataset import Dataset
 from fastargs import Param, Section, get_current_config
 from fastargs.decorators import param
 from imagen_pytorch.t5 import t5_encode_text, DEFAULT_T5_NAME
+from tqdm import tqdm
+
+from dataset_writer_utils import save_tensor
 
 Section("files", "inputs, outputs, etc.").params(
     tags_db=Param(
@@ -14,7 +18,7 @@ Section("files", "inputs, outputs, etc.").params(
         "the sqlite database with id => tag",
         default="./data/danbooru/artifacts/tags.sqlite",
     ),
-    embedding_db=Param(str, "the sqlite database with id => embedding"),
+    out_embedding_db=Param(str, "the sqlite database with id => embedding", default="./data/danbooru/artifacts/embeddings.sqlite"),
     overwrite=Param(bool, "delete the destination db", default=False),
 )
 
@@ -38,39 +42,59 @@ class CaptionDataset(Dataset):
         return self.len
 
     def __getitem__(self, index):
-        text = self.conn.execute(f"SELECT tag FROM tags LIMIT 1 OFFSET {index}").fetchone()
-        return text[0]
+        id, text = self.conn.execute(f"SELECT id, tag FROM tags LIMIT 1 OFFSET {index}").fetchone()
+        return { "id": id, "text": text }
 
 @param("files.tags_db")
-@param("files.embedding_db")
 @param("files.overwrite")
-def run(tags_db, embedding_db, overwrite):
+@param("files.out_embedding_db")
+def run(tags_db, overwrite, out_embedding_db):
     tags_db_path = Path(tags_db)
     assert tags_db_path.is_file(), f"No DB file at: {tags_db}"
 
-    embedding_db_path = Path(embedding_db)
+    embedding_db_path = Path(out_embedding_db)
     if embedding_db_path.exists() and not overwrite:
         print(f"{embedding_db_path} already exists, not overwriting")
         return
     embedding_db_path.unlink(missing_ok=True)
 
-    sqlite_conn = sqlite3.connect(tags_db, check_same_thread=False)
-    rows = sqlite_conn.execute("SELECT COUNT(*) FROM tags").fetchone()[0]
+    tagsdb_conn = sqlite3.connect(tags_db)
+    rows = tagsdb_conn.execute("SELECT COUNT(*) FROM tags").fetchone()[0]
     print(f"Computing embeddings for {rows} texts")
 
-    ds = CaptionDataset(sqlite_conn, rows)
-    dl = DataLoader(ds, batch_size=2, shuffle=False, num_workers=1) 
 
-    for batch in dl:
-        embeds, masks = t5_encode_text(
-            # function expects an array of strings
-            ["hello world baby", "hai"],
+    outdb_conn = sqlite3.connect(out_embedding_db)
+    outdb_conn.execute('pragma journal_mode=wal')
+    outdb_conn.execute("CREATE TABLE embeddings (id INTEGER, text VARCHAR, embedding BLOB)").fetchall()
+
+    ds = CaptionDataset(tagsdb_conn, rows)
+    dl = DataLoader(ds, batch_size=36, shuffle=False, num_workers=1) 
+
+    for batch in tqdm(dl):
+        ids, texts = batch["id"], batch["text"]
+        embeds, mask = t5_encode_text(
+            texts,
             name=DEFAULT_T5_NAME,
             return_attn_mask=True,
         )
-        print(masks)
-        #print((masks == 0).nonzero(as_tuple=True))
-        raise Exception("e")
-        print(embeds.shape)
+        mask = mask.to(torch.int16)
+        rows = []
+        for row_idx, argmin_idx in enumerate(mask.argmin(dim=1)):
+            argmin_idx = argmin_idx.item()
+            embed = None
+            if argmin_idx != 0:
+                embed = embeds[row_idx,:argmin_idx]
+            else:
+                # This happens if a row is [1, 1, 1]
+                # in which case argmin = 1, but
+                # that just means this input generated the most tokens
+                embed = embeds[row_idx,:]
+            embed = embed.cpu()
+
+            embed_bytes = save_tensor(embed)
+            rows.append((ids[row_idx].item(), texts[row_idx], embed_bytes))
+            
+        outdb_conn.executemany("INSERT INTO embeddings VALUES (?, ?, ?)", rows)
+    outdb_conn.execute('VACUUM')
 
 run()
