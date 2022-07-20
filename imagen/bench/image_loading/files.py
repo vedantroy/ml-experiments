d63d24f9a141f6cbcc71162521b66ed5f57ffd60
Vedant Roy <vroy101@gmail.com>
2022-07-19 19:04:12 -0700

    remove useless imagen

--- imagen/bench/image_loading/files.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-imgs_path = "/home/vedant/Desktop/ml-experiments/imagen/data/danbooru/data/dataset/imgs/0000/"
-imgs_path = Path(imgs_path)
-files = list(imgs_path.glob("*"))
-N = 500
-files = files[:N]
