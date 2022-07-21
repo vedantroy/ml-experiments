8c2572e116d972c930bfdd7d99771128dbfefa27
Your Name <you@example.com>
2022-07-21 02:39:33 +0000

    add install script

---
+++ imagen-v2/install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+set -euxo pipefail
+
+curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda_installer.sh
+bash miniconda_installer.sh -b -f -p /home/user/miniconda
+echo 'export PATH="/home/username/miniconda/bin:$PATH"' >> ~/.bashrc
+conda install mamba -n base -c conda-forge
+mamba create -n imagen python=3.9
+pip install -r ./requirements.txt
+conda init bash
+
