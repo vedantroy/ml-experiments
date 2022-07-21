5c834481e7143a31669e1de247b76f0604173c16
Your Name <you@example.com>
2022-07-21 02:41:13 +0000

    update

--- imagen-v2/install.sh
+++ imagen-v2/install.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda_installer.sh
 bash miniconda_installer.sh -b -f -p /home/user/miniconda
-echo 'export PATH="/home/username/miniconda/bin:$PATH"' >> ~/.bashrc
+echo 'export PATH="/home/user/miniconda/bin:$PATH"' >> ~/.bashrc
 conda install mamba -n base -c conda-forge
 mamba create -n imagen python=3.9
 pip install -r ./requirements.txt
